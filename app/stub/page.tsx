8d5b19c8856d35e24a0f3721428200752578d334
engrjeff <engineerjeff@gmail.com>
2023-11-09 17:31:10 +0800

    revert stub page

--- app/stub/page.tsx
+++ app/stub/page.tsx
@@ -1,15 +1,11 @@
 import Stub from "@/components/Stub";
-import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 function StubPage({ searchParams }: { searchParams: { id: string } }) {
-  const cookieStore = cookies();
-  const ygId = cookieStore.get("yg-id");
-
-  if (ygId) redirect("/");
+  if (!searchParams.id) redirect("/");
 
   return (
-    <div className='mt-20 py-10 container px-4 max-w-screen-md min-h-[70vh]'>
+    <div className='mt-20 py-10 container px-4 max-w-screen-md min-h-'>
       <Stub />
     </div>
   );
