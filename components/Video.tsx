7c34d84a08163e39852cc46c11adf7c2ba0fe779
engrjeff <engineerjeff@gmail.com>
2023-11-09 08:23:50 +0800

    make rounded

--- components/Video.tsx
+++ components/Video.tsx
@@ -2,7 +2,7 @@ function Video({ poster, source }: { source: string; poster: string }) {
   return (
     <video
       controls
-      className='w-full aspect-video object-cover'
+      className='w-full aspect-video object-cover rounded-md overflow-hidden'
       poster={poster}
     >
       <source src={source} type='video/mp4' />
